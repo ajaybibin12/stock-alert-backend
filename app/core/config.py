2b372c5bb914f3b9f2e8abfb739c85ee154d4e10
ajaybibin12 <ajaybibin12@gmail.com>
2025-12-14 01:11:01 +0530

    Added Sendgrid email keys into configuration.

--- app/core/config.py
+++ app/core/config.py
@@ -17,10 +17,19 @@ class Settings(BaseSettings):
 
     # ✅ EMAIL SETTINGS (REQUIRED)
     EMAIL_HOST: str
-    EMAIL_PORT: int     # ✅ MUST BE INT
+    EMAIL_PORT: int
     EMAIL_USER: str
     EMAIL_PASS: str
+
+    # ✅ NEW EMAIL SETTINGS FOR SENDGRID
+    # ✅ SENDGRID (NEW)
+    EMAIL_PROVIDER: str | None = "sendgrid"
+    SENDGRID_API_KEY: str | None = None
+    EMAIL_FROM: str | None = None
+
     BACKEND_URL: str
+
+    # ✅ QSTASH SETTINGS
     QSTASH_URL: str
     QSTASH_TOKEN: str
     QSTASH_CURRENT_SIGNING_KEY: str
